b5b0e10af35bacf46d8bd8e0767fc7acc7f6a34a
serr <sc7227484@gmail.com>
2025-11-30 18:00:44 +0300

    quick update

--- config.h
+++ config.h
@@ -5,20 +5,31 @@
 #include "termbox2.h"
 
 /*
-#define TB_DEFAULT              0x0000
-#define TB_BLACK                0x0001
-#define TB_RED                  0x0002
-#define TB_GREEN                0x0003
-#define TB_YELLOW               0x0004
-#define TB_BLUE                 0x0005
-#define TB_MAGENTA              0x0006
-#define TB_CYAN                 0x0007
-#define TB_WHITE                0x0008
+TB_DEFAULT
+TB_BLACK
+TB_RED
+TB_GREEN
+TB_YELLOW
+TB_BLUE
+TB_MAGENTA
+TB_CYAN
+TB_WHITE
 */
 #define TEXT_COLOR           TB_BLUE
 
+/*
+frames per second
+*/
 #define FPS                  24
+
+/*
+large font <-> small font change
+*/
 #define FONT_CHANGE_WIDTH    110
+
+/*
+program exit when min size reached
+*/
 #define MIN_TERMINAL_WIDTH   10
 #define MIN_TERMINAL_HEIGHT  10
 
